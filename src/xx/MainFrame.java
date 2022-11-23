eb7b38fb298ff9a94f04f2a718228860d2f0eb53
Shiye1016 <138666x@gmail.com>
2022-11-23 17:56:18 +0800

    xx

--- src/xx/MainFrame.java
+++ src/xx/MainFrame.java
@@ -12,7 +12,7 @@ public class MainFrame extends JFrame {
         jf.setSize(600,600);
         jf.setLocationRelativeTo(null);
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//设置关闭窗口程序退出
-        jf.setTitle("柿子砍树");
+        jf.setTitle("我是柿子砍树");
         jf.setResizable(false);
         jf.setVisible(true);
     }
