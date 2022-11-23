8691a9d3959acb17e2d73b9d22e9914559cf5a50
Shiye1016 <138666x@gmail.com>
2022-11-23 16:08:45 +0800

    xx

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,8 +7,9 @@ public class MainFrame extends JFrame {
         ImageIcon imageIcon = new ImageIcon("images/123.jpg");
         jf.getContentPane().setBackground(new Color(217, 236, 245)); //设置背景颜色
         jf.setIconImage(imageIcon.getImage());
-        jf.setBounds(Toolkit.getDefaultToolkit().getScreenSize().width/2- jf.getWidth()/2, 0, 600, 600);//获取屏幕宽度并将窗口放置到屏幕最右边
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setSize(600,600);
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//设置关闭窗口程序退出
         jf.setTitle("柿子砍树");
         jf.setResizable(false);
         jf.setVisible(true);
