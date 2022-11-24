5f6af8ace3d3bbd5e868e60c68653c646436eaf8
Shiye1016 <138666x@gmail.com>
2022-11-24 10:32:49 +0800

    xx

--- src/xx/MainFrame.java
+++ src/xx/MainFrame.java
@@ -2,12 +2,11 @@ package xx;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.HashMap;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
         JFrame jf = new JFrame();
-        jf.setLayout(new FlowLayout());
+        jf.setLayout(new FlowLayout());//流动布局
         ImageIcon imageIcon = new ImageIcon("images/123.jpg");
         jf.getContentPane().setBackground(new Color(217, 236, 245)); //设置背景颜色
         jf.setIconImage(imageIcon.getImage());
@@ -15,13 +14,16 @@ public class MainFrame extends JFrame {
         jf.setLocationRelativeTo(null);
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//设置关闭窗口程序退出
         jf.setTitle("我是柿子砍树");
-        jf.setResizable(false);
-
+        //jf.setResizable(false);
         //窗口内容
-        JTextArea text= new JTextArea(2,4);
-        text.setBounds(0,0,100,20);
-        text.setFont(new Font("微软雅黑",Font.PLAIN,19));
-        jf.getContentPane().add(text);
+        //JTextArea text= new JTextArea(2,4);
+        //text.setBounds(0,0,100,20);
+        //text.setFont(new Font("微软雅黑",Font.PLAIN,19));
+       // jf.getContentPane().add(text);
+        JTextField te = new JTextField(null,null,20);
+        te.setLocation(0,0);
+        te.setBounds(0,0,200,10);
+        jf.getContentPane().add(te);
         jf.setVisible(true);
     }
     public static void main(String[] args){
