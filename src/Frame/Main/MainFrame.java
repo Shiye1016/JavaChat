47ad82d8845432a3b91bea5b2726de22b3c25bec
xx <138666x@gmail.com>
2022-11-26 00:28:50 +0800

    xx

--- src/Frame/Main/MainFrame.java
+++ src/Frame/Main/MainFrame.java
@@ -1,10 +1,6 @@
 package Frame.Main;
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 import Constants.Constants;
 
@@ -15,23 +11,27 @@ public class MainFrame extends JFrame {
     public void Init(){
         this.setSize(width,height);
         this.getContentPane().setBackground(new Color(217, 236, 245)); //背景颜色
-        this.setIconImage(new ImageIcon("images/123.jpg").getImage());//窗口图标
         this.setLocationRelativeTo(null);//窗口居中显示
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//关闭窗口程序退出
         this.setTitle(Constants.APP_NAME);//窗口标题
         this.setResizable(false);
-        this.setVisible(true);
+        this.setIconImage(new ImageIcon("images/TIcon.jpg").getImage());//窗口图标
+
         //左上角头像
         JLabel IconLabel = new JLabel();
-        IconLabel.setIcon(new ImageIcon("images/123.jpg"));
-        Box Icon =Box.createHorizontalBox();//水平排列
+
+        IconLabel.setIcon(new ImageIcon("images/1233.png"));
+        IconLabel.setSize(100,100);
+        Box Icon =Box.createVerticalBox();//水平排列
         Icon.add(IconLabel);
+        Icon.setSize(100,100);
         this.add(Icon);
-
-
+        this.setVisible(true);
     }
+
     public static void main(String[] args){
-        new MainFrame().Init();
+            new MainFrame().Init();
     }
+
 }
 
