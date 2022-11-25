b25607b898baf9754af56f9a0c89bb649ecf4340
xx <138666x@gmail.com>
2022-11-25 23:53:48 +0800

    xx

--- src/Frame/Main/MainFrame.java
+++ src/Frame/Main/MainFrame.java
@@ -13,19 +13,19 @@ public class MainFrame extends JFrame {
     final int height = 700;
     public MainFrame() {}
     public void Init(){
-        setSize(width,height);
-        getContentPane().setBackground(new Color(217, 236, 245)); //背景颜色
-        setIconImage(new ImageIcon("images//123.jpg").getImage());//窗口图标
-        setLocationRelativeTo(null);//窗口居中显示
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//关闭窗口程序退出
-        setTitle(Constants.APP_NAME);//窗口标题
-        setResizable(false);
-        setVisible(true);
+        this.setSize(width,height);
+        this.getContentPane().setBackground(new Color(217, 236, 245)); //背景颜色
+        this.setIconImage(new ImageIcon("images/123.jpg").getImage());//窗口图标
+        this.setLocationRelativeTo(null);//窗口居中显示
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//关闭窗口程序退出
+        this.setTitle(Constants.APP_NAME);//窗口标题
+        this.setResizable(false);
+        this.setVisible(true);
         //左上角头像
         JLabel IconLabel = new JLabel();
-        this.add(IconLabel);
+        IconLabel.setIcon(new ImageIcon("images/123.jpg"));
         Box Icon =Box.createHorizontalBox();//水平排列
-
+        Icon.add(IconLabel);
         this.add(Icon);
 
 
