7935bfa811b5b272b893f7760f6f48cebf832853
Shiye1016 <138666x@gmail.com>
2022-11-26 17:34:23 +0800

    xx

--- src/Frame/Main/MainFrame.java
+++ src/Frame/Main/MainFrame.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     final int width = 1000;
-    final int height = 700;
+    final int height = 550;
     public MainFrame() {}
     public void Init(){
         this.setSize(width,height);
@@ -25,9 +25,8 @@ public class MainFrame extends JFrame {
         IconBox.add(IconLabel);
         //头像下用户名
         JLabel uName = new JLabel("默认用户名");
-        uName.setFont(new Font("微软雅黑", Font.BOLD,14));
+        uName.setFont(new Font("微软雅黑", Font.BOLD,20));
         IconBox.add(Box.createVerticalStrut(10));
-        //IconBox.add(Box.createHorizontalStrut(10));
         IconBox.add(uName);
         IconBox.setBounds(60,30,100,135);
         //组装用户名和头像
@@ -37,16 +36,30 @@ public class MainFrame extends JFrame {
         panel.add(IconBox);
 
 
-        //头像下列表框
-        Box friendsList = Box.createVerticalBox();
-
+        //聊天列表框
         String[] item = {"好友1","好友2","好友3","好友4","好友5"};
-        JList<String> jl_2=new JList<>(item);// 创建列表框
-        panel.add(jl_2);
-        jl_2.setBounds(40,160,150,150); //列表框在panel中的位置
+        JList<String> jl_1=new JList<>(item);// 创建列表框
+        JLabel listText = new JLabel("聊天列表");
+        panel.add(listText);
+        listText.setBounds(80,100,200,200);
+        panel.add(jl_1);
+        jl_1.setBounds(60-50,210,100+50*2+10,200);
+
+
+        //添加删除按钮
+        Box bBox = Box.createHorizontalBox();
+        JButton add = new JButton("添加好友");
+        JButton del = new JButton("删除好友");
+        bBox.add(add);
+        bBox.add(Box.createHorizontalStrut(10));
+        bBox.add(del);
+        panel.add(bBox);
+        bBox.setBounds(23,350,200,200);
+
 
         this.add(panel);
         this.setVisible(true);
+
     }
 
     public static void main(String[] args){
