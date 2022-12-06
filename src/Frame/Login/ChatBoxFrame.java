84e5e0ebb22622655d2fb589ba763903ecfc711e
Shiye1016 <138666x@gmail.com>
2022-12-06 20:44:07 +0800

    xx

--- src/Frame/Login/ChatBoxFrame.java
+++ src/Frame/Login/ChatBoxFrame.java
@@ -62,7 +62,7 @@ public class ChatBoxFrame{
                 String password=pField.getText().trim();
                 //访问登陆接口
 
-            }//0000
+            }//0000bbbbb
         });
 
         btnBox.add(loginBtn);
