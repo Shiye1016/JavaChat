ebbbad80993b85af03df04ed924e810251555927
Coco9 <718520134@qq.com>
2022-12-09 20:46:13 +0800

    1

--- src/Login/RegisteredFrame.java
+++ src/Login/RegisteredFrame.java
@@ -5,10 +5,25 @@ import javax.swing.*;
 public class RegisteredFrame extends JFrame{
     private static int height=480;
     private static int width=640;
-    private static JButton=new JButton("确认");
-    private static int width=640;
+    private static JButton regsbtn=new JButton("注册");
+    private static JButton cancelbtn=new JButton("取消");
     public void init(){
     this.setSize(width,height);
+    //主BOX
+    Box mainBox=Box.createVerticalBox();
+
+    //
+
+
+
+    //按钮
+    Box uBox= Box.createHorizontalBox();
+    uBox.add(regsbtn);
+    uBox.add(Box.createHorizontalStrut(60));
+    uBox.add(cancelbtn);
+
+
+
 
     this.setLocationRelativeTo(null);
     this.setResizable(false);
